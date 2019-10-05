2ebd26c574e6d5afa366f89c9fadbbe056c576f3
Pedro Fernandes <pedromiguel.fernandes17@gmail.com>
2019-10-05 14:24:59 +0100

    Adding dashboard template

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
 
 const App = () => {
   return (
     <div className="App">
-      <Navbar />
+      <Dashboard />
     </div>
   );
 };
---
+++ client/src/components/layout/Appbar.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Badge from '@material-ui/core/Badge';
+import MenuIcon from '@material-ui/icons/Menu';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+
+const Appbar = ({ classes, open, handleDrawerOpen }) => (
+  <AppBar
+    position="absolute"
+    className={clsx(classes.appBar, open && classes.appBarShift)}
+  >
+    <Toolbar className={classes.toolbar}>
+      <IconButton
+        edge="start"
+        color="inherit"
+        aria-label="open drawer"
+        onClick={handleDrawerOpen}
+        className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+      >
+        <MenuIcon />
+      </IconButton>
+      <Typography
+        component="h1"
+        variant="h6"
+        color="inherit"
+        noWrap
+        className={classes.title}
+      >
+        360º Company Dashboard
+      </Typography>
+      <IconButton color="inherit">
+        <Badge badgeContent={4} color="secondary">
+          <NotificationsIcon />
+        </Badge>
+      </IconButton>
+    </Toolbar>
+  </AppBar>
+);
+
+Appbar.propTypes = {
+  classes: PropTypes.node.isRequired,
+  open: PropTypes.bool.isRequired,
+  handleDrawerOpen: PropTypes.func.isRequired,
+};
+
+export default Appbar;
