811cbf70ba277fc322b92bec2a7daae4cf684e11
Pedro Fernandes <pedromiguel.fernandes17@gmail.com>
2019-10-05 18:38:02 +0100

    Adding Log In page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Dashboard from './components/dashboard/Dashboard';
+import SignIn from './components/SignIn';
 
 const App = () => {
   return (
-    <div className="App">
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Dashboard />
+          </Route>
+          <Route exact path="/login">
+            <SignIn />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
