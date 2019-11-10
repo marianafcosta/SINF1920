60d1e069e7bf81128280b34ee3b2233fd24c890b
Pedro Fernandes <pedromiguel.fernandes17@gmail.com>
2019-11-10 22:21:31 +0000

    login and logout working

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,51 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/authActions';
 
 import Dashboard from './components/dashboard/Dashboard';
 import SignIn from './components/SignIn';
 
 const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  });
+
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path="/">
-            <Dashboard />
-          </Route>
-          <Route exact path="/login">
-            <SignIn />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={({ location }) =>
+                store.getState().auth.isAuthenticated ? (
+                  <Dashboard />
+                ) : (
+                  <Redirect
+                    to={{
+                      pathname: '/login',
+                      state: { from: location },
+                    }}
+                  />
+                )
+              }
+            />
+            <Route exact path="/login">
+              <SignIn />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 };
 
