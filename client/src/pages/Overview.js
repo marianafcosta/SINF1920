06eea77f092256f3baa573c484deacf6d403585e
Mariana Filipa Costa <up201604414@fe.up.pt>
2019-11-23 12:52:32 +0000

    fix(fe): disable import linting

--- client/src/pages/Overview.js
+++ client/src/pages/Overview.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Layout from '../components/layout/Layout';
+// eslint-disable-next-line
 import CustomCard from '../components/customCard/CustomCard';
 
 const Overview = () => (
   <Layout>
-    <CustomCard title="Sales vs Expenses" />
+    <CustomCard
+      title="Sales vs Expenses"
+      overlayInfo="testing overlay descriptions"
+    />
   </Layout>
 );
 
