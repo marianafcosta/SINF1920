a7aca2bd2ced825f25b0c2662cafa572e96d7557
Mariana Filipa Costa <up201604414@fe.up.pt>
2019-12-04 22:04:23 +0000

    feat(fe): add initial files

--- client/src/pages/Financial.js
+++ client/src/pages/Financial.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Layout from '../components/layout/Layout';
 
+import Ebitda from '../components/ebitda';
+
 const Financial = () => (
   <Layout>
-    <h1>HI</h1>
+    <Ebitda />
   </Layout>
 );
 
