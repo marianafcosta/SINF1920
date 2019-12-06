8dc044d25ed85ac60586d3c5b66b498daceb5426
Mariana Filipa Costa <up201604414@fe.up.pt>
2019-12-06 16:30:50 +0000

    feat(server): calculate total liabilities, assets and equity

--- financial.js
+++ financial.js
@@ -251,6 +251,191 @@ const calculateEquity = accounts => {
   return totalCapital;
 };
 
+const calculateAssets = accounts => {
+  /*
+  'assets': {
+      'Ativo não corrente': {
+          'Ativos fixos tangíveis': 0, //43+453+455-459 ✓
+          'Propriedades de investimento': 0, //42+455+452-459 ✓
+          'Ativos intangíveis': 0, //44+454+455-459 ✓
+          'Investimentos financeiros': 0, //41 ✓
+          'Accionistas/Sócios': 0 //266 + 268 - 269 ✓
+      },
+      'Ativo corrente': {
+          'Inventários': 0, //32+33+34+35+36+39 ✓
+          'Clientes': 0, //211+212-219 ✓
+          'Adiantamentos a fornecedores': 0, //228-229+2713-279 ✓
+          'Estado e outros entes públicos': 0, //24 ✓
+          'Accioninistas/Sócios': 0, //263+268-269 ✓
+          'Outras Contas a Receber': 0, //232+238-239+2721+278-279 ✓
+          'Diferimentos': 0, //281 ✓
+          'Outros ativos financeiros': 0, //14 ✓
+          'Caixa e depósitos bancários': 0 // 11+12+13 ✓
+      },
+      'Total do ativo': 0,
+  }, 
+  */
+
+  const currentAssetsCalculations = [
+    [32, 33, 34, 35, 36, 39],
+    [211, 212, -219],
+    [228, -229, 2713, 279],
+    [24],
+    [263, 268, -269],
+    [232, 238, -239, 2721, 278, -279],
+    [281],
+    [14],
+    [11, 12, 13],
+  ];
+
+  const nonCurrentAssetsCalculations = [
+    [43, 453, 455, -459],
+    [42, 455, 452, -459],
+    [44, 454, 455, -459],
+    [41],
+    [266, 268, -269],
+  ];
+
+  let total = 0;
+  let currentAccount;
+  let sum = 0;
+  nonCurrentAssetsCalculations.forEach(asset => {
+    sum = 0;
+    asset.forEach(account => {
+      currentAccount = fetchAccount(accounts, Math.abs(account));
+      if (account < 0) {
+        sum -=
+          currentAccount.ClosingDebitBalance -
+          currentAccount.ClosingCreditBalance;
+      } else {
+        sum +=
+          currentAccount.ClosingDebitBalance -
+          currentAccount.ClosingCreditBalance;
+      }
+    });
+    total += sum;
+  });
+  const nonCurrentAssets = total;
+
+  total = 0;
+  currentAssetsCalculations.forEach(asset => {
+    sum = 0;
+    asset.forEach(account => {
+      currentAccount = fetchAccount(accounts, Math.abs(account));
+      if (currentAccount) {
+        if (account < 0) {
+          sum -=
+            currentAccount.ClosingDebitBalance -
+            currentAccount.ClosingCreditBalance;
+        } else {
+          sum +=
+            currentAccount.ClosingDebitBalance -
+            currentAccount.ClosingCreditBalance;
+        }
+      }
+    });
+    total += sum;
+  });
+
+  const currentAssets = total;
+
+  console.log('non current');
+  console.log(nonCurrentAssets);
+  console.log('current');
+  console.log(currentAssets);
+
+  return nonCurrentAssets + currentAssets;
+};
+
+const calculateLiabilities = accounts => {
+  /**
+   * 'liabilities': {
+        'Passivo não corrente': {
+            'Provisões': 0, //29 ✓
+            'Financiamentos obtidos': 0, //25 ✓
+            'Outras contas a pagar': 0 //237+2711+2712+275 ✓
+        },
+        'Passivo corrente': {
+            'Fornecedores': 0, //221+222+225 ✓
+            'Adiantamentos de clientes': 0, //218+276 ✓
+            'Estado e outros entes públicos': 0, //24 ✓
+            'Accionistas/Sócios': 0, //264+265+268 ✓
+            'Financiamentos obtidos': 0, //25 ✓
+            'Outras contas a pagar': 0, //231+238+2711+2712+2722+278 ✓
+            'Diferimentos': 0, //282+283
+            'Outros passivos financeiros': 0 //14 ✓
+        },
+   */
+  const currentLiabilitiesCalculations = [
+    [221, 222, 225],
+    [218, 27],
+    [24],
+    [264, 265, 268],
+    [25],
+    [231, 238, 2711, 2712, 2722, 278],
+    [282, 283],
+    [14],
+  ];
+
+  const nonCurrentLiabilitiesCalculations = [
+    [29],
+    [25],
+    [237, 2711, 2712, 275],
+  ];
+
+  let total = 0;
+  let currentAccount;
+  let sum = 0;
+  nonCurrentLiabilitiesCalculations.forEach(asset => {
+    sum = 0;
+    asset.forEach(account => {
+      currentAccount = fetchAccount(accounts, Math.abs(account));
+      if (currentAccount) {
+        if (account < 0) {
+          sum -=
+            currentAccount.ClosingDebitBalance -
+            currentAccount.ClosingCreditBalance;
+        } else {
+          sum +=
+            currentAccount.ClosingDebitBalance -
+            currentAccount.ClosingCreditBalance;
+        }
+      }
+    });
+    total += sum;
+  });
+  const nonCurrentLiability = total;
+
+  total = 0;
+  currentLiabilitiesCalculations.forEach(asset => {
+    sum = 0;
+    asset.forEach(account => {
+      currentAccount = fetchAccount(accounts, Math.abs(account));
+      if (currentAccount) {
+        if (account < 0) {
+          sum -=
+            currentAccount.ClosingDebitBalance -
+            currentAccount.ClosingCreditBalance;
+        } else {
+          sum +=
+            currentAccount.ClosingDebitBalance -
+            currentAccount.ClosingCreditBalance;
+        }
+      }
+    });
+    total += sum;
+  });
+
+  const currentLiability = total;
+
+  console.log('non current');
+  console.log(nonCurrentLiability);
+  console.log('current');
+  console.log(currentLiability);
+
+  return nonCurrentLiability + currentLiability;
+};
+
 module.exports = (server, db) => {
   /**
    * @param year (required)
@@ -299,5 +484,17 @@ module.exports = (server, db) => {
     const accounts = db.MasterFiles.GeneralLedgerAccounts.Account;
     const equity = calculateEquity(accounts);
     res.json(equity);
+  });
+
+  server.get('/api/financial/assets', (req, res) => {
+    const accounts = db.MasterFiles.GeneralLedgerAccounts.Account;
+    const assets = calculateAssets(accounts);
+    res.json(assets);
+  });
+
+  server.get('/api/financial/liabilities', (req, res) => {
+    const accounts = db.MasterFiles.GeneralLedgerAccounts.Account;
+    const liabilities = calculateLiabilities(accounts);
+    res.json(liabilities);
   });
 };
