57986107543a0dcd9733f843d42fd0bea48b6501
Mariana Filipa Costa <up201604414@fe.up.pt>
2019-12-07 13:46:36 +0000

    feat(liquidity): implement component

--- client/src/components/liquidityRatios/LiquidityRatios.js
+++ client/src/components/liquidityRatios/LiquidityRatios.js
@@ -1,7 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import CustomCard from '../CustomCard';
+import TableCard from '../TableCard';
+
+import {
+  fetchWorkingCapital,
+  fetchCashRatio,
+  fetchCurrentRatio,
+} from '../../services/financialService';
+
+const headers = [
+  { name: 'cash', label: 'Cash ratio' },
+  { name: 'current', label: 'Current ratio' },
+  { name: 'workingCapital', label: 'Working capital (€)' },
+];
 
 const LiquidityRatios = () => {
-  return <h1>dfkjsdfsa</h1>;
+  const [ratios, setRatios] = useState({
+    cash: 0,
+    workingCapital: 0,
+    current: 0,
+  });
+  const [tableData, setTableData] = useState([]);
+
+  const fetchData = async () => {
+    const cashResponse = await fetchCashRatio();
+    const workingCapitalResponse = await fetchWorkingCapital();
+    const currentResponse = await fetchCurrentRatio();
+    console.log(cashResponse);
+    console.log(workingCapitalResponse);
+    console.log(currentResponse);
+    setRatios({
+      cash: cashResponse.data,
+      workingCapital: workingCapitalResponse.data,
+      current: currentResponse.data,
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (ratios) {
+      console.log(ratios);
+      setTableData([
+        {
+          cash: ratios.cash,
+          current: ratios.current,
+          workingCapital: ratios.workingCapital,
+        },
+      ]);
+    }
+  }, [ratios]);
+  return (
+    <CustomCard title="Liquidity Ratios" overlayInfo="G E M P">
+      <TableCard headers={headers} data={tableData} />
+    </CustomCard>
+  );
 };
 
 export default LiquidityRatios;
--- client/src/pages/Financial.js
+++ client/src/pages/Financial.js
@@ -3,11 +3,13 @@ import Layout from '../components/layout/Layout';
 
 import Ebitda from '../components/ebitda';
 import AccountsReceivable from '../components/accountsReceivable';
+import LiquidityRatios from '../components/liquidityRatios';
 
 const Financial = () => (
   <Layout>
     <Ebitda />
     <AccountsReceivable />
+    <LiquidityRatios />
   </Layout>
 );
 
