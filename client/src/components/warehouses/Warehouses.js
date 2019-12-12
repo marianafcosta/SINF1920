3ca370fc613097fc14d42aa8fb4722a1a894169e
Mariana Filipa Costa <up201604414@fe.up.pt>
2019-12-12 10:16:19 +0000

    feat(warehouses): add list of warehouses  and their current value in stock

---
+++ client/src/components/warehouses/Warehouses.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+
+import CustomCard from '../CustomCard';
+import TableCard from '../TableCard';
+
+import { fetchWarehouses } from '../../services/inventoryService';
+
+const headers = [
+  { name: 'id', label: 'ID' },
+  { name: 'name', label: 'Name' },
+  { name: 'amount', label: 'Current amount (€)' },
+];
+
+const Warehouses = () => {
+  const [tableData, setTableData] = useState([]);
+
+  const fetchData = async () => {
+    const { data } = await fetchWarehouses();
+    setTableData(
+      data.map(({ id, name, amount }) => ({
+        id,
+        name,
+        amount,
+      })),
+    );
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <CustomCard title="Warehouses" overlayInfo="a place to store things">
+      <TableCard headers={headers} data={tableData} />
+    </CustomCard>
+  );
+};
+
+export default Warehouses;
--- client/src/pages/Inventory.js
+++ client/src/pages/Inventory.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Layout from '../components/layout/Layout';
 import Products from '../components/inventory/products';
 import Stock from '../components/inventory/stock';
+import Warehouses from '../components/warehouses';
 
 const Inventory = () => (
   <Layout pageName="Inventory">
     <Products title="Products" />
     <Stock />
+    <Warehouses />
   </Layout>
 );
 
