035df7325169f72e4914b381c1488bc7f9d3844c
Mariana Filipa Costa <up201604414@fe.up.pt>
2019-12-12 21:53:04 +0000

    feat(purchases): fetch purchases from a client from server

--- client/src/components/customerPurchases/CustomerPurchases.js
+++ client/src/components/customerPurchases/CustomerPurchases.js
@@ -1,33 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import numeral from 'numeral';
 
 import CustomCard from '../CustomCard';
 import TableCard from '../TableCard';
 
+import { fetchPurchases } from '../../services/customerService';
+
 const headers = [
-  { name: 'id', label: 'ID' },
+  { name: 'id', label: 'ID', link: 'products' },
   { name: 'name', label: 'Name' },
   { name: 'units', label: 'Units' },
+  { name: 'value', label: 'Value' },
 ];
 
 const CustomerPurchases = ({ customerId }) => {
   const [purchases, setPurchases] = useState([]);
 
-  const fetchData = () => {
-    /*
-      response: [
-        {
-            id: product id,
-            name: product name,
-            units: units acquired
-        }
-      ]
-      */
+  const fetchData = async () => {
+    const { data } = await fetchPurchases(customerId, 2018);
+    setPurchases(
+      data.map(item => ({
+        ...item,
+        units: numeral(item.units).format('0a'),
+        value: numeral(item.value).format('0.000a'),
+      })),
+    );
   };
 
   useEffect(() => {
     fetchData();
-  });
+  }, []);
 
   return (
     <CustomCard title="Purchases" overlayInfo="dkfngçsdasfsd">
---
+++ client/src/services/customerService.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const baseUrl = '/api/customers';
+
+//eslint-disable-next-line
+const fetchPurchases = (customerId, year) =>
+  axios.get(`${baseUrl}/${customerId}/purchases`); // TODO year
+
+//eslint-disable-next-line
+export { fetchPurchases };
