2d99228735980ce85307037e51fb7e1c4e213b60
Mariana Filipa Costa <up201604414@fe.up.pt>
2019-12-12 11:28:42 +0000

    feat(ebit): add ebit component and fix the year specified in some of the calls

--- client/src/pages/Financial.js
+++ client/src/pages/Financial.js
@@ -3,6 +3,7 @@ import Layout from '../components/layout/Layout';
 
 import StabilityRatios from '../components/stabilityRatios';
 import Ebitda from '../components/ebitda';
+import Ebit from '../components/ebit';
 import AccountsReceivable from '../components/accountsReceivable';
 import LiquidityRatios from '../components/liquidityRatios';
 import SalesAndExpenses from '../components/salesAndExpenses';
@@ -11,6 +12,7 @@ const Financial = () => (
   <Layout pageName="Financial">
     <StabilityRatios />
     <Ebitda />
+    <Ebit />
     <AccountsReceivable />
     <LiquidityRatios />
     <SalesAndExpenses />
