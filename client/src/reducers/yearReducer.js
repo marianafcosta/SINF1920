1780259cfbd4e0a37fa27aa1ca1b4944d6e4a725
Pedro Silva <up201604470@fe.up.pt>
2019-12-13 09:48:56 +0000

    Fix error in redux

--- client/src/reducers/yearReducer.js
+++ client/src/reducers/yearReducer.js
@@ -1,11 +1,11 @@
-import { FISCAL_YEAR } from '../actions/types';
+import { SET_FISCAL_YEAR } from '../actions/types';
 
 const initialState = 2018;
 
 export default function(state = initialState, action) {
 
   switch(action.type) {
-    case FISCAL_YEAR:
+    case SET_FISCAL_YEAR:
       return action.payload;
     default:
       return state;
